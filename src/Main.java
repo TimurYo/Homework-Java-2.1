9fb96868e5df451253e99af99b34f2466f248db8
SabirovTimur <sabirovtimurcfc@gmail.com>
2023-01-31 14:57:27 +0300

    Initaial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int TotalAmount = 68_000;
+        int AmountOfMoneyForFreeMiles = 20;
+
+        System.out.println("Количество бонусных миль:");
+        System.out.println((TotalAmount / AmountOfMoneyForFreeMiles) + " миль");
+
+    }
+}
+
+
